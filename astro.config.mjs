cae715f17272dbd5c006288350f80b0d6971ccee
davnpsh <git@davnpsh.dev>
2024-08-11 23:02:03 -0500

    change: adding base to fix root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://giusgo.github.io",
+  base: "data-mining",
   integrations: [
     react(),
     tailwind({
