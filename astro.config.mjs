6cc1cbe85c18cdb0b4fdb02fdc4b90db50f68f34
davnpsh <git@davnpsh.dev>
2024-08-11 22:59:44 -0500

    feat: github pages auto-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://giusgo.github.io",
   integrations: [
     react(),
     tailwind({
